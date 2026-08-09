57d7a8d6bbf45a1cac54240e794bef52ac596c97
abhishek-230506 <br4261947@gmail.com>
2026-08-09 21:56:11 +0530

    final_1

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='/assassin.jpeg' alt='Abhishek B R' />
+          <img src='/abhishek.jpeg' alt='Abhishek B R' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
